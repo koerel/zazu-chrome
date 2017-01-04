f21e2e1745af16e724f7e3e15b11aa80b8c9ee93
Karel Eeckelaert <karel@bagaar.be>
2017-01-04 12:03:58 +0100

    Add chrome integration

--- src/open.js
+++ src/open.js
@@ -1,17 +1,14 @@
 module.exports = (pluginContext) => {
   return {
     respondsTo: (query) => {
-      return query.match(/hello.*/)
+      return query.match(/,c.*/)
     },
     search: (query, env = {}) => {
-      const name = query.split(/\s+/)[1] || ''
-      const title = name ? `Hello, ${name}!` : 'Hello!'
+      const url = query.split(/\s+/)[1] || ''
       return new Promise((resolve, reject) => {
         resolve([
           {
-            icon: 'fa-hand-spock-o',
-            title,
-            subtitle: 'Please type your name!',
+            value: url,
           }
         ])
       })
