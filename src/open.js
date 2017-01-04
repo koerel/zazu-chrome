5e16d99a8a650f3b8656996b2008abe9d8baa188
Karel Eeckelaert <karel@bagaar.be>
2017-01-04 13:21:04 +0100

    test

--- src/open.js
+++ src/open.js
@@ -3,13 +3,14 @@
 module.exports = (pluginContext) => {
   return (query, env) => {
     const variables = env || {}
+    const value = query.includes('.') ? `http://${value}` : `https://google.com?q=${query}`;
     return new Promise((resolve, reject) => {
       resolve([
         {
           id: query,
           title: query,
           subtitle: query,
-          value: query,
+          value: value,
         }
       ])
     })
