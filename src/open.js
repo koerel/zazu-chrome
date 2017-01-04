0cf01569ca46198899711dcc84a882e1698af2fe
Karel Eeckelaert <karel@bagaar.be>
2017-01-04 12:18:00 +0100

    Simplify

--- src/open.js
+++ src/open.js
@@ -1,17 +1,5 @@
 module.exports = (pluginContext) => {
-  return {
-    respondsTo: (query) => {
-      return query.match(/,c.*/)
-    },
-    search: (query, env = {}) => {
-      const url = query.split(/\s+/)[1] || ''
-      return new Promise((resolve, reject) => {
-        resolve([
-          {
-            value: url,
-          }
-        ])
-      })
-    },
-  }
+  return (query) => {
+        return query
+    }
 }
